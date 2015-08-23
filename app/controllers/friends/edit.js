31fcd55573f94503c40fe7e6d1bdaf558ddf2009
Martin Rodriguez <merodriguezblanco@gmail.com>
2015-08-23 19:37:57 -0300

    Use deactivate on friends routes

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -1,6 +1,10 @@
 import FriendsBaseController from './base';
 
 export default FriendsBaseController.extend({
+  deactivate: function () {
+    var model = this.modelFor('friends/edit');
+    model.rollback();
+  },
   actions: {
     cancel: function () {
       this.transitionToRoute('friends.show', this.get('model'));
